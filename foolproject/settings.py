602190b3e349148f1a9745948b57408d43ede1b1
cromtus <cromtus@yandex.ru>
2020-03-22 21:24:06 +0300

    restruct

--- foolproject/settings.py
+++ foolproject/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/frontend/www/'
 
 
 MEDIA_URL = "/media/"
