adef81a7f98bb8d971c635df7db7ad2426f7b0ca
Vitaly Krasnov <krasnov.vv@phystech.edu>
2020-03-23 17:39:55 +0300

    add api for getting marked memes

--- memes/urls.py
+++ memes/urls.py
@@ -1,9 +1,11 @@
 from rest_framework import routers
 from .api import MemesViewSet
 from .api import UnMarkedMemesViewSet
+from .api import MarkedMemesViewSet
 
 
 router = routers.DefaultRouter()
 router.register('api/memes', MemesViewSet, 'memes')
 router.register('api/unmarkedmemes', UnMarkedMemesViewSet, 'unmarkedmemes')
+router.register('api/markedmemes', MarkedMemesViewSet, 'markedmemes')
 urlpatterns = router.urls
