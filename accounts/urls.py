2bd207e53475796f9677b542e417060f5cf154aa
Vitaly Krasnov <krasnov.vv@phystech.edu>
2020-04-14 19:16:23 +0300

    add API for getting user list for Danya

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .api import RegisterAPI, LoginAPI, UserAPI
+from .api import RegisterAPI, LoginAPI, UserAPI, UsersViewSet
 from knox import views as knox_views
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('api/auth/login', LoginAPI.as_view()),
     path('api/auth/user', UserAPI.as_view()),
     path('api/auth/logout', knox_views.LogoutView.as_view(), name='knox_logout'),
+    path('api/userList', UsersViewSet.as_view({'get': 'list'}))
 ]
