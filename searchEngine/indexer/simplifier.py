f16b7574af04f55f6a954dc2247c7eeb9102b4ac
Vitaly Krasnov <krasnov.vv@phystech.edu>
2020-04-16 12:19:34 +0300

    add cheking on empty string in simplifier

--- searchEngine/indexer/simplifier.py
+++ searchEngine/indexer/simplifier.py
@@ -5,22 +5,23 @@ SIMPLIFIED_CHARACTERS = {'о': 'а', 'ё': 'и', 'е': 'и', 'з': 'с', 'ъ': '
 
 
 def simplify_string(ustr):
-    pattern = re.compile(r'[\W_]+')
-    result = str(ustr).lower()
-    temp = pattern.sub(' ', result)
+    if ustr is not None and ustr != '':
+        pattern = re.compile(r'[\W_]+')
+        result = str(ustr).lower()
+        temp = pattern.sub(' ', result)
 
-    words = temp.split(' ')
-    res_words = []
-    for word in words:
-        if len(word) > 3:
-            for ch in SIMPLIFIED_CHARACTERS:
-                word = word.replace(ch, SIMPLIFIED_CHARACTERS[ch])
-        res_words.append(word)
+        words = temp.split(' ')
+        res_words = []
+        for word in words:
+            if len(word) > 3:
+                for ch in SIMPLIFIED_CHARACTERS:
+                    word = word.replace(ch, SIMPLIFIED_CHARACTERS[ch])
+            res_words.append(word)
 
-    res = " ".join(res_words).replace('ё', 'и').replace('й', 'и') # костылим
-    if res[0] == ' ':
-        res = res[1:]
-    if res[-1] == ' ':
-        res = res[:-1]
+        res = " ".join(res_words).replace('ё', 'и').replace('й', 'и') # костылим
+        if res[0] == ' ':
+            res = res[1:]
+        if res[-1] == ' ':
+            res = res[:-1]
 
-    return res
+        return res
