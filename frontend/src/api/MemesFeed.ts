6868cfd9763abe6620453e4d6473f83192550f55
Artem Levashov <levashov.as@phystech.edu>
2020-05-05 18:48:51 +0300

    Tags filter frontend

--- frontend/src/api/MemesFeed.ts
+++ frontend/src/api/MemesFeed.ts
@@ -1,10 +1,11 @@
 import Axios from 'axios'
 import { FeedMeme } from '../util/Types'
 
-export async function memesFeedApi(filter: "rating" | "time" | "ratio", tags: number[], it: number): Promise<FeedMeme[]> {
+export async function memesFeedApi(filter: "rating" | "time" | "ratio", plusTags: number[], minusTags: number[], it: number): Promise<FeedMeme[]> {
   const urlFeed = new URLSearchParams()
   urlFeed.append('filter', filter)
-  urlFeed.append('tags', tags.join())
+  urlFeed.append('tags', plusTags.join())
+  urlFeed.append('ban', minusTags.join())
   urlFeed.append('it', it.toString())
   return (await Axios.post<FeedMeme[]>('api/wall' + '?' + urlFeed)).data
 }
