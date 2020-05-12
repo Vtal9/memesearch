ea4fd95f3f77f603f66ea4419c8a134d604e703d
Artem Levashov <levashov.as@phystech.edu>
2020-05-12 19:10:38 +0300

    Allocation tracing

--- markupUnmarked.py
+++ markupUnmarked.py
@@ -2,6 +2,8 @@ import os
 import django
 import torch
 import sys
+import linecache
+import tracemalloc
 
 from config import settings
 
@@ -16,11 +18,36 @@ from memes.ocr_pipeline.craft import CRAFT
 from memes.ocr_pipeline.recognition import netInference, copyStateDict
 
 
+def display_top(snapshot, key_type='lineno', limit=5):
+    snapshot = snapshot.filter_traces((
+        tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
+        tracemalloc.Filter(False, "<unknown>"),
+    ))
+    top_stats = snapshot.statistics(key_type)
+
+    print("Top %s lines" % limit)
+    for index, stat in enumerate(top_stats[:limit], 1):
+        frame = stat.traceback[0]
+        print("#%s: %s:%s: %.1f KiB"
+              % (index, frame.filename, frame.lineno, stat.size / 1024))
+        line = linecache.getline(frame.filename, frame.lineno).strip()
+        if line:
+            print('    %s' % line)
+
+    other = top_stats[limit:]
+    if other:
+        size = sum(stat.size for stat in other)
+        print("%s other: %.1f KiB" % (len(other), size / 1024))
+    total = sum(stat.size for stat in top_stats)
+    print("Total allocated size: %.1f KiB" % (total / 1024))
+
+
 def markUp(n=500):
 
     # ImageDescriptions.objects.all().delete()
     # TextDescriptions.objects.all().delete()
 
+    tracemalloc.start()
     net = CRAFT()
     net.load_state_dict(copyStateDict(torch.load("memes/ocr_pipeline/craft_mlt_25k.pth", map_location='cpu')))
     net.eval()
@@ -42,6 +69,8 @@ def markUp(n=500):
 
         meme.is_mark_up_added = False
         meme.save()  # при сохранении индекс сам обновляется для этого мема
+        snapshot = tracemalloc.take_snapshot()
+        display_top(snapshot)
 
 
 if __name__ == '__main__':
