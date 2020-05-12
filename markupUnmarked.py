0149e0d4270b63bf3c85f3187503e8bfeda8abe3
Artem Levashov <levashov.as@phystech.edu>
2020-05-12 19:27:14 +0300

    Inspecting variables size in markup

--- markupUnmarked.py
+++ markupUnmarked.py
@@ -2,8 +2,6 @@ import os
 import django
 import torch
 import sys
-import linecache
-import tracemalloc
 
 from config import settings
 
@@ -18,50 +16,28 @@ from memes.ocr_pipeline.craft import CRAFT
 from memes.ocr_pipeline.recognition import netInference, copyStateDict
 
 
-def display_top(snapshot, key_type='lineno', limit=5):
-    snapshot = snapshot.filter_traces((
-        tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
-        tracemalloc.Filter(False, "<unknown>"),
-    ))
-    top_stats = snapshot.statistics(key_type)
-
-    print("Top %s lines" % limit)
-    for index, stat in enumerate(top_stats[:limit], 1):
-        frame = stat.traceback[0]
-        print("#%s: %s:%s: %.1f KiB"
-              % (index, frame.filename, frame.lineno, stat.size / 1024))
-        line = linecache.getline(frame.filename, frame.lineno).strip()
-        if line:
-            print('    %s' % line)
-
-    other = top_stats[limit:]
-    if other:
-        size = sum(stat.size for stat in other)
-        print("%s other: %.1f KiB" % (len(other), size / 1024))
-    total = sum(stat.size for stat in top_stats)
-    print("Total allocated size: %.1f KiB" % (total / 1024))
-
-
 def markUp(n=500):
 
     # ImageDescriptions.objects.all().delete()
     # TextDescriptions.objects.all().delete()
 
-    tracemalloc.start()
     net = CRAFT()
     net.load_state_dict(copyStateDict(torch.load("memes/ocr_pipeline/craft_mlt_25k.pth", map_location='cpu')))
     net.eval()
 
+    y = settings.Y
     for _ in range(n):
+        print('net size', sys.getsizeof(net))
         try:
             meme = Memes.objects.filter(textDescription="").order_by('?')[0]
         except Exception as e:
             break
         print("memeID: " + str(meme.id))
-        y = settings.Y
         path = 'media/toMarkup'   # по идее по этому адрсу и будет лежать картинка, можешь поменять адрес как тебе надо
         y.download(meme.fileName, path)
+        print('y size', sys.getsizeof(y))
         textDescription = " ".join(netInference(net, path))
+        print('text descr size', sys.getsizeof(textDescription))
         if os.path.isfile(path):
             os.remove(path)
         # Построение нового индекса по добавленному мему
@@ -69,8 +45,7 @@ def markUp(n=500):
 
         meme.is_mark_up_added = False
         meme.save()  # при сохранении индекс сам обновляется для этого мема
-        snapshot = tracemalloc.take_snapshot()
-        display_top(snapshot)
+        print('meme size', sys.getsizeof(meme))
 
 
 if __name__ == '__main__':
